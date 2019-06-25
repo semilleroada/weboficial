fc9d0265bc1b6b76d7ab576f4ea42fe64cbb881d
AndresEsala <andresesala25@gmail.com>
2019-06-24 21:15:57 -0500

    Mejora de Diseño

--- WebPageSchool/js/main.js
+++ WebPageSchool/js/main.js
@@ -4,13 +4,11 @@ $(function () {
 	/* Menu Movil Desplegable: Dropdown */
 	$('.movil-nav').on('click', function(){
 		$('.sub-ul').slideToggle();
+		$('div.areas').slideUp();
 	})
 
 	/* Menu Desplegable de Áreas: DropDown */
 	$('header .sub-ul li:first-child a').on('click', function() {
 		$('.areas').slideToggle();
 	})
-
-	/* Divs de las Áreas */
-	let divAreas = document.querySelector('.areas');
 });
